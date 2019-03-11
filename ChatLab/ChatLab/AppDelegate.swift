5ca0a6978ac4bae1bc76018d800a2e26f3260634
Nestor Hernandez <nestor.jbautista@gmail.com>
2019-03-10 20:50:42 -0600

    Test commit from master

--- ChatLab/ChatLab/AppDelegate.swift
+++ ChatLab/ChatLab/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static var navigationCoordinator: NavigationCoordinatorProtocol!
     static var dependencyRegistry: DependencyRegistryProtocol!
 
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
